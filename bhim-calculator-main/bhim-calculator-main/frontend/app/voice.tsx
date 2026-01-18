ba82f957480e6945eb39af26c0e8af04fa613ebb
vishalmsk5 <vishalmsk5@gmail.com>
2026-01-19 01:19:00 +0530

    Update BACKEND_URL for voice.tsx

--- bhim-calculator-main/bhim-calculator-main/frontend/app/voice.tsx
+++ bhim-calculator-main/bhim-calculator-main/frontend/app/voice.tsx
@@ -22,7 +22,8 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 //const BACKEND_URL = process.env.EXPO_PUBLIC_BACKEND_URL;
 //const BACKEND_URL = "http://127.0.0.1:8000"; // PC IP on same WiFi
-const BACKEND_URL = "https://bhim-universal-calculator.onrender.com/api";
+//const BACKEND_URL = "https://bhim-universal-calculator.onrender.com/api";
+const BACKEND_URL = "https://bhim-universal-calculator.onrender.com";
 export default function VoicePage() {
   const router = useRouter();
   const { theme } = useThemeStore();
